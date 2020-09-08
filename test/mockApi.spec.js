340b948821416ae339e898b57ce12902756146fb
vapatty6848 <vapatty@gmail.com>
2020-09-08 19:11:25 -0300

    test mockApi ok

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -18,12 +18,25 @@ Note que as informações devem estar de acordo com o JSON
 presente no README.md do projeto.
 
 Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
+const url = 'https://randomuser.me/api/';
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
-
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+  const user = {
+    gender: 'male',
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
+    },
+    location: {country: 'Brazil'},
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+    }
+  api.fetchURL = jest.fn().mockImplementation(() => Promise.resolve(user));
 
   test('verifica se o usuário é o tunico', async () => {
     return api.fetchURL().then((user) => {
