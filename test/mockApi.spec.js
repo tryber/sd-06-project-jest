5079670a29126e86182463ff3be3a014a925c621
higoramorim <1810042@aluno.univesp.br>
2020-09-08 20:20:10 -0300

    mokiApi.spec.js

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,7 +23,26 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  const x = jest.fn().
+  api.fetchURL = jest.fn().mockImplementation(() => {
+    return new Promise(function (resolve){
+      const user = {
+        gender: 'male',
+        name: {
+          first: 'Antônio',
+          last: 'Britto'
+        },
+        location: {
+          country: 'Brazil',
+        },
+        email: 'tunico@bol.com.br',
+        login: {
+          username: 'tunicao123',
+          password: '1234567890',
+        },
+      };
+      resolve(user);
+    });
+  });
 
   test('verifica se o usuário é o tunico', async () => {
     return api.fetchURL().then((user) => {
