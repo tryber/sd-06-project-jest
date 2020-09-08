4965fab39f10712830fa8323b7216cd32724821a
Ana Higo <anahigo.arq@gmail.com>
2020-09-08 17:52:06 -0300

    atualizando projeto

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -22,8 +22,18 @@ Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
+
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  const tunico = {
+    gender: 'male',
+    name: { first: 'Antônio', last: 'Britto' },
+    location: { country: 'Brazil' },
+    email: 'tunico@bol.com.br',
+    login: { username: 'tunicao123', password: '1234567890' },
+  };
+  
+  api.fetchURL = jest.fn().mockResolvedValue(tunico);
 
   test('verifica se o usuário é o tunico', async () => {
     return api.fetchURL().then((user) => {
@@ -37,3 +47,24 @@ describe('verifica o usuário', () => {
     });
   });
 });
+
+/*
+Resposta Esperada
+const https = require('https');
+
+const url = 'https://randomuser.me/api/';
+
+const fetchURL = () => new Promise(function (resolve, reject) {
+  https.get(url, (res) => {
+    res.setEncoding('utf8');
+    let userInfo = '';
+    res.on('data', (data) => {
+      userInfo += data;
+    });
+    res.on('error', reject);
+    res.on('close', () => resolve(JSON.parse(userInfo).results[0]));
+  });
+});
+
+module.exports = { fetchURL };
+*/
