e8359c944929ab342aa560391c95aaf1c723a464
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-09-08 18:13:15 -0300

    Fix issues

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,17 +24,17 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   const requestReturn = {
-      gender: 'male',
-      name: { title: 'Mr', first: 'Antônio', last: 'Britto' },
-      location: {
-        country: 'Brazil',
-      },
-      email: 'tunico@bol.com.br',
-      login: {
-        username: 'tunicao123',
-        password: '1234567890',
-      },
-    }
+    gender: 'male',
+    name: { title: 'Mr', first: 'Antônio', last: 'Britto' },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  };
 
   test('verifica se o usuário é o tunico', async () => {
     api.fetchURL = jest.fn().mockResolvedValue(requestReturn);
