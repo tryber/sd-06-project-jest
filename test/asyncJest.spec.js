13804be663da8f23d4e597db0dc59a5147e0dff5
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-09-08 16:23:55 -0300

    teste code climat

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -10,7 +10,7 @@ a função recebe como parâmetro true e false, respectivamente.
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
-describe("o retorno do telefonema", () => {
+describe("o retorno do telefonema", () => {  
   test("atende", () => {
     // assert.fail();
     // Insira seu teste assíncrono aqui
