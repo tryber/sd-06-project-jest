ca8af6d363102a5d699940eb7f82bc91f89200fc
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-09-08 17:56:16 -0300

    test mockApi testes funcionais

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,6 +24,26 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  const Tunicao = new Promise((resolve) => {
+    resolve(
+      {
+        gender: 'male',
+        name: {
+          first: 'Antônio',
+          last: 'Britto',
+        },
+        location: {
+          country: 'Brazil',
+        },
+        email: 'tunico@bol.com.br',
+        login: {
+          username: 'tunicao123',
+          password: '1234567890',
+        },
+      }
+    )
+  });
+  api.fetchURL = jest.fn().mockReturnValue(Tunicao);
 
   test('verifica se o usuário é o tunico', async () => {
     return api.fetchURL().then((user) => {
