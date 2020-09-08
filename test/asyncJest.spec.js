5a5d72c236d84b38f57b920323d582c704748a98
Anderson Meira <contatodeanderson@gmail.com>
2020-09-08 18:09:01 -0300

    Requisito Jest Assincrono

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu choro assíncrono aqui
+  test("atende", async () => {
+    const resultado = await answerPhone(true)
+    expect(resultado).toEqual('Oi!');
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira suas lágrimas assíncronas aqui
+  test("ocupado", async () => {
+    try {
+      await answerPhone(false);
+    } catch (error) {  
+      expect(error).toEqual('Infelizmente não podemos atender...');
+    }
   });
 });
