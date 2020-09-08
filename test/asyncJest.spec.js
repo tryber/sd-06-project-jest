763da5c1f8536b65251a9532179039d97559aea0
Rods <rods.leite27@gmail.com>
2020-09-08 16:35:51 -0300

    Requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,4 @@
-const answerPhone = require("../src/asyncJest");
+let answerPhone = require("../src/asyncJest");
 
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -12,11 +12,11 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui.
+    let answerPhone = jest.fn().mockImplementation(()=> 'Oi!');
+    expect(answerPhone()).toBe('Oi!');
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    let answerPhone = jest.fn().mockImplementation(()=> 'Infelizmente não podemos atender...');
+    expect(answerPhone()).toBe('Infelizmente não podemos atender...');
   });
 });
