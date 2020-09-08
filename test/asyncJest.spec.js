30baee3be30196ccd5e90420f4970e31425fc18d
rafaelgelatti <rafael.gelatti@gmail.com>
2020-09-08 16:56:21 -0300

    First Commit

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    return answerPhone('Alô!?').then((answer) => {
+      expect(answer).toMatch('Oi!');
+    });
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    return answerPhone().catch((error) => {
+      expect(error).toMatch('Infelizmente não podemos atender...');
+    });
   });
 });
