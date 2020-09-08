fdc329140ab261fd79368a2c2a6c321d18940324
Artur Forna <fornagieri.artur@gmail.com>
2020-09-08 15:59:08 -0300

    Iniciando o projeto :suspect:

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,12 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
+    // assert.fail();
+    expect(answerPhone()).resolves.toBe('Oi!');
     // Insira seu teste assíncrono aqui
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    // assert.fail();
+    expect(answerPhone()).resolves.toBe('Infelizmente não podemos atender...');
   });
 });
