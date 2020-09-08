258a795cd2b2f0aeafdb77422c859e250ff2bf80
Anderson <andersoni.trybe@gmail.com>
2020-09-08 18:18:56 -0300

    Req 3 done

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -1,4 +1,5 @@
 const api = require('../src/mockApi');
+const mockApi = require('../src/mockApi');
 
 /*
 A função fetchURL retorna um JSON com informações de um usuário aleatório buscadas da API 'randomuser.me'.
@@ -24,6 +25,17 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  api.fetchURL = jest.fn();
+  api.fetchURL = () => new Promise((resolve) => {
+    const tunico = {
+      gender: 'male',
+      name: {first: 'Antônio', last: 'Britto'},
+      location: {country: 'Brazil'},
+      email: 'tunico@bol.com.br',
+      login: {username: 'tunicao123',  password: '1234567890'}
+      };
+      resolve(tunico);
+  });
 
   test('verifica se o usuário é o tunico', async () => {
     return api.fetchURL().then((user) => {
