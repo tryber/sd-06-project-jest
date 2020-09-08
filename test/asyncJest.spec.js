7ca80021c252af6b98ed3ba9933a5428fa34caea
Macarena Rodriguez <macarenar697@gmail.com>
2020-09-08 17:01:49 -0300

    Complete os testes do arquivo test/asyncJest.spec.js para que funcionem com código assíncrono. faltou o return para esperar apromise

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,10 +12,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    expect(answerPhone(true)).resolves.toBe('Oi!');
+    return expect(answerPhone(true)).resolves.toBe('Oi!');
   });
 
   test("ocupado", () => {
-    expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
+    return expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
   });
 });
