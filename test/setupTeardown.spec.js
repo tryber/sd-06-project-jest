db11449a78cd20a5fc4f3a8acca47381e3eda732
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-09-08 18:43:18 -0300

    4th requirement - adding  comments

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -21,7 +21,8 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('quem sobreviveu?', () => {
   beforeEach(() => adventure.randomAttack());
-
+  // usando beforeEach, antes de cada funcao abaixo ser testada
+  // sera chamada a funcao original adventure.randomAttack()
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
   });
