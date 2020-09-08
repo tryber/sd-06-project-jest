4855f1afa27624d59f4005945777384531f52ddc
Cássio Rubens <cassiorubensadv@gmail.com>
2020-09-08 17:46:54 -0300

    Requisito Jest Assíncrono

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
+    // assert.fail();
     // Insira seu teste assíncrono aqui
+    expect(answerPhone(true)).resolves.toMatch('Oi!');
   });
   test("ocupado", () => {
-    assert.fail();
+    // assert.fail();
     // Insira seu teste assíncrono aqui
+    expect(answerPhone(false)).rejects.toMatch('Infelizmente não podemos atender...');
   });
 });
