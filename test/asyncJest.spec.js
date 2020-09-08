83d80f574eca2f7631ef7c9313a74cc520adf994
Pablo Santos <pablo.trybe@gmail.com>
2020-09-08 15:42:53 -0300

    First commit, initializing project

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -10,13 +10,13 @@ a função recebe como parâmetro true e false, respectivamente.
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
-describe("o retorno do telefonema", () => {
+describe("o retorno do telefonema", done => {
   test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect(answerPhone).toBe('Oi');
+    done();
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect(answerPhone).toBe('Infelizmente não podemos atender...');
+    done();
   });
 });
