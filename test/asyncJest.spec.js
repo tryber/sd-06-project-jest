e738b8fd3edc7921139a7c73fbcabcea0a3f92f5
Tiago Berwanger <berwangertiago@gmail.com>
2020-09-08 16:25:29 -0300

    requisito 1 - completo

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,15 +11,11 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  it("atende", () => {
+    return expect(answerPhone(true)).resolves.toBe('Oi!');
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+
+  it("ocupado", () => {
+    return expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
   });
 });
-
-
-//iniciando o projeto...
