2dda703083714e35bcba1010c1648c4bffd3cb56
Alessandra <itsalematos@hotmail.com>
2020-09-08 15:17:27 -0400

    Add primeiro requisito - testes assíncronos

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,16 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("atende",  async () => {
+    // assert.fail();
+    answerPhone.answerPhone = jest.fn().mockResolvedValue('Oi!');
+    expect(answerPhone.answerPhone(true)).resolves.toBe('Oi!');
+
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+
+  test("ocupado", async () => {
+    // assert.fail();
+    answerPhone.answerPhone = jest.fn().mockRejectedValue('Infelizmente não podemos atender...');
+    expect(answerPhone.answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
   });
 });
