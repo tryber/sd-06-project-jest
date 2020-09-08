f3ed6e25f657150828f75bd53dab8d813ea0f7cc
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-09-08 18:28:47 -0300

    2nd requirement

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -14,7 +14,11 @@ describe("o retorno do telefonema", () => {
   test("atende", () => {
     expect(answerPhone(true)).resolves.toBe('Oi!');
   });
+  // aqui consideramos a opcao true de retorno da answerPhone e usamos
+  // o .resolves nessa promise que volta como resolvida
   test("ocupado", () => {
     expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
   });
+    // aqui consideramos a opcao false de retorno da answerPhone e usamos
+  // o .rejects nessa promise que volta como falha
 });
--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,22 +1,22 @@
 const mockFunctions = require('../src/mockFunctions');
-
-/*
-Criamos uma série de funções com eficiência duvidosa.
-Elas estão no arquivo 'src/mockFunctions.js'.
-Crie mock functions para cada uma das operações de modo que os cálculos sejam feitos corretamente,
-não como estão sendo feitos no arquivo original.
-A idéia é que os novos testes sobrescrevam os testes
-importados apenas na suite de testes abaixo.
-
-Importante! A correção de código via mock functions não é uma aplicação usual.
-O foco aqui é a utilização de mock functions.
-
-ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
-*/
+jest.mock('../src/mockFunctions');
+// aqui com jest.mock(src) conseguimos mockar todo o pacote, simulando
+// todas as funcoes de uma vez, portanto, dentro do describe so foi
+// necessario escrever como seriam as funcoes mockadas.
 
 describe('verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
-  
+  mockFunctions.add.mockImplementation((a, b) => a + b);
+  mockFunctions.subtract.mockImplementation((a, b) => a - b);
+  mockFunctions.multiply.mockImplementation((a, b) => a * b);
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
+  mockFunctions.power.mockImplementation((a, b) => a ** b);
+  mockFunctions.factorial.mockImplementation((a) => {
+    let result = 1;
+    for (let i=1; i <= a; i += 1) {
+      result *= i;
+    }
+    return result;
+  });
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
     expect(mockFunctions.add(8, 37)).toEqual(45);
