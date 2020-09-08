2dc11a4492f64f9a28917a31200f6f82b911a362
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-09-08 17:37:19 -0300

    completa o primeiro requisito

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -10,13 +10,11 @@ a função recebe como parâmetro true e false, respectivamente.
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
-describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+describe('o retorno do telefonema', () => {
+  it('atende', () => {
+    return expect(answerPhone(true)).resolves.toBe('Oi!');
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  it('ocupado', () => {
+    return expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...');
   });
 });
