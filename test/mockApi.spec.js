b30ade0a3a2303028814a1d25189ca18aeef8558
paulo-lins <paulolins100@gmail.com>
2020-09-09 00:31:30 -0300

    mockApi.spec.js

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,6 +24,35 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  const user = {
+    gender: 'male',
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
+    },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  };
+  api.fetchURL = jest.fn(() => Promise.resolve(
+    user;
+  ));
+  // const fetchURL = () => new Promise(function (resolve, reject) {
+  //   https.get(url, (res) => {
+  //     res.setEncoding('utf8');
+  //     let userInfo = '';
+  //     res.on('data', (data) => {
+  //       userInfo += data;
+  //     });
+  //     res.on('error', reject);
+  //     res.on('close', () => resolve(JSON.parse(userInfo).results[0]));
+  //   });
+  // });
 
   test('verifica se o usuário é o tunico', async () => {
     return api.fetchURL().then((user) => {
