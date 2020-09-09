1d1686ef78ae075bcce76c81164942626fd04ee8
StoneHg <leticia.bora@gmail.com>
2020-09-09 14:34:48 -0300

    teste cc

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -7,34 +7,34 @@ Após cada aventura um de nossos aventureiros cairá.
 Cada um dos testes abaixo verifica a quantidade de aventureiros após cada iteração.
 Sua missão aqui é:
 
-  - Use a função randomAttack do objeto adventure
-  que remove um dos aventureiros toda vez que é chamada,
-  ela deve funcionar entre cada teste.
-  Opcional:
-  - Para ficar mais visível, imprima na tela após cada teste o grupo de aventureiros restante.
-  - No fim dos testes, imprima uma mensagem com o nome do aventureiro que sobreviveu.
+- Use a função randomAttack do objeto adventure
+que remove um dos aventureiros toda vez que é chamada,
+ela deve funcionar entre cada teste.
+Opcional:
+- Para ficar mais visível, imprima na tela após cada teste o grupo de aventureiros restante.
+- No fim dos testes, imprima uma mensagem com o nome do aventureiro que sobreviveu.
 
 PS: Os codinomes dos aventureiros são reais! Tentem descobrir quem é quem!
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
-describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
-
-  test('depois da primeira aventura', () => {
-    expect(adventure.specialists.length).toBe(5);
-  });
-  test('depois da segunda aventura', () => {
-    expect(adventure.specialists.length).toBe(4);
-  });
-  test('depois da terceira aventura', () => {
-    expect(adventure.specialists.length).toBe(3);
-  });
-  test('depois da quarta aventura', () => {
-    expect(adventure.specialists.length).toBe(2);
-  });
-  test('depois da quinta aventura', () => {
-    expect(adventure.specialists.length).toBe(1);
-  });
-});
+describe('quem sobreviveu?', () => { beforeEach(() => adventure.randomAttack()) 
+test('depois da primeira aventura', () => {
+expect(adventure.specialists.length).toBe(5);
+})
+test('depois da segunda aventura', () => {
+expect(adventure.specialists.length).toBe(4);
+console.log('testeeeee', adventure.specialists)
+})
+test('depois da terceira aventura', () => {
+expect(adventure.specialists.length).toBe(3);
+console.log('testeeeee', adventure.specialists)
+})
+test('depois da quarta aventura', () => {
+expect(adventure.specialists.length).toBe(2);
+console.log('testeeeee', adventure.specialists)
+})
+test('depois da quinta aventura', () => {expect(adventure.specialists.length).toBe(1);
+})
+})
