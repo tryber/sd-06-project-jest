c463452b242e3fe9b938a48c87dae458e7ca690b
Daniel Jesus <dev.danieljs@gmail.com>
2020-09-08 21:41:10 -0300

    Adding fourth requirement: setupTearDown

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -1,4 +1,5 @@
 const adventure = require('../src/setupTeardown');
+const { specialists } = require('../src/setupTeardown');
 /*
 Num universo não tão distante, um grupo de aventureiros da Trybe enfrentam uma série de testes.
 O grupo parte em direção ao sucesso, mas,
@@ -20,7 +21,17 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('quem sobreviveu?', () => {
-  // Adicione seu código aqui
+  beforeEach(() => {
+    adventure.randomAttack();
+  });
+
+  afterEach(() => {
+    console.log(`Survived, ${adventure.specialists[0].nome}...`);
+
+    const survivingAdventurerGroup;
+    survivingAdventurerGroup = adventure.specialists.map(group => group.nome);
+    console.log(`Surviving's group: ${survivingAdventurerGroup} `);
+  });
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
