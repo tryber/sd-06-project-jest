827d7cca42def0b3977c302425e8654d68deba13
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-09-08 23:46:11 -0300

    Adding last requirements

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -17,7 +17,6 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica as funções e os mocks', () => {
-
   mockFunctions.add.mockImplementation((a, b) => a + b);
   mockFunctions.subtract.mockImplementation((a, b) => a - b);
   mockFunctions.divide.mockImplementation((a, b) => a / b);
@@ -31,7 +30,6 @@ describe('verifica as funções e os mocks', () => {
     return fact;
   })
 
-  
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
     expect(mockFunctions.add(8, 37)).toEqual(45);
