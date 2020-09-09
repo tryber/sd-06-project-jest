0e347e80188aa0a10aee960b7e966019920b3404
Pedro <pedroedu77@gmail.com>
2020-09-09 19:39:09 -0300

    Requisito 4 concluido

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -35,7 +35,7 @@ describe('verifica o usuário', () => {
     email: 'tunico@bol.com.br',
     login: {
       username: 'tunicao123',
-      password: '1234567890',
+      password: '1234567890'
     }
   };
   const apiURL = jest.spyOn(api, 'fetchURL');
