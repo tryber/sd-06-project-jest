8cf1df7959429a84d655f693aab37b21139e2f01
Igor Leão <igorleao.eng@gmail.com>
2020-09-09 08:03:48 -0300

    testing uppdate

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,9 +23,6 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
-  
-
   const expectedUser = {
     gender: 'male',
     name: {
