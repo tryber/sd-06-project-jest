1338727a6603b243f79bbb1915b30ae2bee3c8c3
Pedro <pedroedu77@gmail.com>
2020-09-09 19:22:54 -0300

    Requisito 3 concluido

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -16,7 +16,6 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
   mockFunctions.multiply.mockImplementation((a, b) => a * b);
   mockFunctions.add.mockImplementation((a, b) => a + b);
   mockFunctions.divide.mockImplementation((a, b) => a / b);
